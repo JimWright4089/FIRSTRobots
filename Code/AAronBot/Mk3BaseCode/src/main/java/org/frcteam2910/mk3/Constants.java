3c51afdddeb1026df75e6438a110a39ca5904f2d
Jim Wright <james@jimwright.org>
2021-05-11 20:50:42 -0700

    Added Aaron bot and Romi

---
+++ Code/AAronBot/Mk3BaseCode/src/main/java/org/frcteam2910/mk3/Constants.java
@@ -0,0 +1,37 @@
+package org.frcteam2910.mk3;
+
+public class Constants {
+  public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 20;
+  public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 21;
+  public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 22;
+  public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 23;
+
+  public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 24;
+  public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 25;
+  public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 26;
+  public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 27;
+
+  public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 10;
+  public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 11;
+  public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 12;
+  public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 13;
+
+  public static final int PIGEON_PORT = 51;
+
+  // In degrees
+//  public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = Math.toRadians(285.0659);
+  public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = Math.toRadians(195.0659);
+//  public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = Math.toRadians(196.3970);
+  public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = Math.toRadians(166.397);
+//  public static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET = Math.toRadians(309.1853);
+  public static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET = Math.toRadians(129.1853);
+//  public static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET = Math.toRadians(348.0489);
+  public static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET = Math.toRadians(78.0489);
+
+  public static final int PRIMARY_CONTROLLER_PORT = 0;
+
+  public static final double JOYSTICK_X = 0.0;
+  public static final double JOYSTICK_Y = 0.0;
+
+
+}
